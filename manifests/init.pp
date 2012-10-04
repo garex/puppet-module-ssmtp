e33138786e7b1198936afdb6d3b183b5493400f5
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2012-10-04 17:35:17 +0700

    Auth params and use flags

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,20 @@
 class ssmtp (
-  $mailhub
+  $mailhub,
+
+  # Auth with login/password
+  $authUser     = undef,
+  $authPass     = undef,
+  $authMethod   = 'login',    # login, cram-md5
+  $useOldAUTH   = false,
+  $useTLS       = false,
+  $useSTARTTLS  = false,
+  $useTLSCert   = false,
+  $TLSCert      = '/dev/null' # <path to your cert>
 ) {
 
   package {"Installing sSMTP package":
-    name    => "ssmtp",
-    ensure  => "present"
+    name      => "ssmtp",
+    ensure    => "present"
   }
 
   file {
