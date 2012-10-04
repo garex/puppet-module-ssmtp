1b65ebc12461dd49bd58acc654eab154373089d9
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2012-10-04 16:39:34 +0700

    Most simplest config -- just mailhub

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,18 @@
-class ssmtp {
+class ssmtp (
+  $mailhub
+) {
 
   package {"Installing sSMTP package":
     name    => "ssmtp",
     ensure  => "present"
+  }
+
+  file {
+    "Main sSMTP config":
+      require => Package["Installing sSMTP package"],
+      ensure  => "present",
+      path    => "/etc/ssmtp/ssmtp.conf",
+      content => template("ssmtp/ssmtp.conf.erb")
   }
 
 }
